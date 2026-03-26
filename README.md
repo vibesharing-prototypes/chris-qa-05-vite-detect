8e1f1814c36424f5b1547ad48a9e6849b23e23cf
Vibesharing-App <chris_avore@me.com>
2026-03-26 07:42:08 -0400

    QA test

--- README.md
+++
@@ -1,2 +0,0 @@
-# chris-qa-05-vite-detect
-Prototype: QA-05 Vite Detect - Deployed via VibeSharing
---
+++ src/App.tsx
@@ -0,0 +1,2 @@
+import { AppLayout } from "@diligentcorp/atlas-react-bundle";
+export default function App(){return <AppLayout><h1>Vite</h1></AppLayout>}
